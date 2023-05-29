300f1052de645d664dc947a92d5808ebc17160d2
leonardoborgesLeotacotroid <leonardo.borges.pech.180104@gmail.com>
2023-05-28 19:07:56 -0600

    beta contabilidad

--- contabilidad/beta.py
+++ contabilidad/beta.py
@@ -7,7 +7,6 @@ from tkcalendar import DateEntry
 class VentanaPrincipal(CTk):
     global T
     T = 0
-
     def __init__(self):
             super().__init__()
             self.title("Ejemplo de ComboBox y Treeview")
@@ -18,17 +17,18 @@ class VentanaPrincipal(CTk):
             self.combobox.pack(pady=10)
 
             # Crear Treeview
-            self.treeview = ttk.Treeview(self, columns=(0, 1, 2, 3, 4, 5, 6, 7, 8), show="headings")
+            self.treeview = ttk.Treeview(self, columns=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), show="headings")
             self.treeview.pack()
             self.treeview.heading(0, text="Fecha de operacion")
-            self.treeview.heading(1, text="Entradas")
-            self.treeview.heading(2, text="Salidas")
-            self.treeview.heading(3, text="Existencias")
-            self.treeview.heading(4, text="Costo unitario")
-            self.treeview.heading(5, text="Costo promedio")
-            self.treeview.heading(6, text="Debe")
-            self.treeview.heading(7, text="Haber")
-            self.treeview.heading(8, text="Saldo")
+            self.treeview.heading(1, text="Tipo de Operacion")
+            self.treeview.heading(2, text="Entradas")
+            self.treeview.heading(3, text="Salidas")
+            self.treeview.heading(4, text="Existencias")
+            self.treeview.heading(5, text="Costo Unitario")
+            self.treeview.heading(6, text="Costo Promedio")
+            self.treeview.heading(7, text="Debe")
+            self.treeview.heading(8, text="Haber")
+            self.treeview.heading(9, text="Saldo")
 
             # Crear botón para abrir ventana
             self.btn_abrir_ventana = CTkButton(self, text="Abrir Ventana", command=self.abrir_ventana)
@@ -88,15 +88,15 @@ class VentanaOpcion1(tk.Toplevel):
 
         # Crear campos de entrada
         vcmd = (self.register(self.validate_entry), '%P')
-        self.label_col1 = CTkLabel(self, text="Entradas:", text_color="Black")
-        self.label_col1.grid(row=0, column=0, padx=5, pady=5)
-        self.entry_col1 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col1.grid(row=0, column=1, padx=5, pady=5)
+        self.label_col2 = CTkLabel(self, text="Entradas:", text_color="Black")
+        self.label_col2.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_col2 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col2.grid(row=0, column=1, padx=5, pady=5)
 
-        self.label_col4 = CTkLabel(self, text="Costo unitario:", text_color="Black")
-        self.label_col4.grid(row=1, column=0, padx=5, pady=5)
-        self.entry_col4 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col4.grid(row=1, column=1, padx=5, pady=5)
+        self.label_col5 = CTkLabel(self, text="Costo unitario:", text_color="Black")
+        self.label_col5.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_col5 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col5.grid(row=1, column=1, padx=5, pady=5)
 
         # Crear campo de fecha
         self.label_fecha = CTkLabel(self, text="Fecha:", text_color="Black")
@@ -110,36 +110,29 @@ class VentanaOpcion1(tk.Toplevel):
 
     def guardar_datos(self):
         # Verificar que los campos no estén vacíos y que cumplan con las condiciones especificadas
-        if not all([self.entry_col1.get(), self.entry_col4.get()]):
+        if not all([self.entry_col2.get(), self.entry_col5.get()]):
             messagebox.showerror("Error", "Ningún campo debe estar vacío.", parent=self)
             return
-
-        if not all([self.validate_entry(self.entry_col1.get()), self.validate_entry(self.entry_col4.get())]):
-            messagebox.showerror("Error",
-                                 "Los campos deben cumplir con las condiciones especificadas en el método validate_entry.",
-                                 parent=self)
-            return
-        col1 = float(self.entry_col1.get())
-        col4 = float(self.entry_col4.get())
-        col2 = 0
-        col3 = col1
-        col5 = 0
-        col6 = float(col1) * float(col4)
-        col7 = 0
-        col8 = col6
-
-        # Get the selected date from the DateEntry widget
+        col1 = "INICIO"
+        col2 = float(self.entry_col2.get())
+        col3 = 0
+        col4 = col2
+        col5 = float(self.entry_col5.get())
+        col6 = 0
+        col7 = float(col2) * float(col5)
+        col8 = "-----"
+        col9 = col7  # Get the selected date from the DateEntry widget
         selected_date = self.date_entry.get_date()
 
         # Format the date as a string
         formatted_date = selected_date.strftime("%Y-%m-%d")  # Customize the format as desired
 
-        # Lógica para obtener col5
-        if col3 != 0:
-            col5 = float(col8) / float(col3)
+        # Lógica para obtener col6
+        if col4 != 0:
+            col6 = float(col9) / float(col4)
         # Agregar datos al Treeview en la ventana principal
         ventana_principal = self.master
-        ventana_principal.treeview.insert("", "end", values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8))
+        ventana_principal.treeview.insert("", "end", values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8,col9))
 
         self.destroy()
 
@@ -147,7 +140,7 @@ class VentanaOpcion1(tk.Toplevel):
         if not new_text:
             return True
         try:
-            if int(new_text) > 0 and int(new_text) <= 1000:
+            if int(new_text) > 0 and int(new_text) <= 10000:
                 return True
             else:
                 return False
@@ -161,15 +154,15 @@ class VentanaOpcion2(tk.Toplevel):
 
         # Crear campos de entrada
         vcmd = (self.register(self.validate_entry), '%P')
-        self.label_col1 = CTkLabel(self, text="Entradas:", text_color="Black")
-        self.label_col1.grid(row=0, column=0, padx=5, pady=5)
-        self.entry_col1 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col1.grid(row=0, column=1, padx=5, pady=5)
+        self.label_col2 = CTkLabel(self, text="Entradas:", text_color="Black")
+        self.label_col2.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_col2 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col2.grid(row=0, column=1, padx=5, pady=5)
 
-        self.label_col4 = CTkLabel(self, text="Costo unitario:", text_color="Black")
-        self.label_col4.grid(row=1, column=0, padx=5, pady=5)
-        self.entry_col4 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col4.grid(row=1, column=1, padx=5, pady=5)
+        self.label_col5 = CTkLabel(self, text="Costo unitario:", text_color="Black")
+        self.label_col5.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_col5 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col5.grid(row=1, column=1, padx=5, pady=5)
 
         # Crear campo de fecha
         self.label_fecha = CTkLabel(self, text="Fecha:", text_color="Black")
@@ -182,33 +175,33 @@ class VentanaOpcion2(tk.Toplevel):
         self.btn_guardar.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
     def guardar_datos(self):
-        if not all([self.entry_col1.get(), self.entry_col4.get()]):
+        if not all([self.entry_col2.get(), self.entry_col5.get()]):
             messagebox.showerror("Error", "Ningún campo debe estar vacío.", parent=self)
             return
         selected_item = self.master.treeview.focus()
         if not selected_item:
             messagebox.showerror("Error", "Debe seleccionar una fila.", parent=self)
             return
-
-        col1 = self.entry_col1.get()
-        col4 = self.entry_col4.get()
-        col2 = 0
-        col3 = float(col1) + float(self.master.treeview.item(self.master.treeview.focus())["values"][3])
-        col5 = 0
-        col6 = float(col1) * float(col4)
-        col7 = 0
-        col8 = col6 + float(self.master.treeview.item(self.master.treeview.focus())["values"][8])
+        col1= "COMPRA"
+        col2 = self.entry_col2.get()
+        col5 = self.entry_col5.get()
+        col3 = 0
+        col4 = float(col2) + float(self.master.treeview.item(self.master.treeview.focus())["values"][4])
+        col6 = 0
+        col7 = float(col2) * float(col5)
+        col8 = 0
+        col9 = col7 + float(self.master.treeview.item(self.master.treeview.focus())["values"][9])
 
         # Get the selected date from the DateEntry widget
         selected_date = self.date_entry.get_date()
         formatted_date = selected_date.strftime("%Y-%m-%d")  # Customize the format as desired
         # Lógica para obtener col5
-        if col3 != 0:
-            col5 = float(col8) / float(col3)
+        if col4 != 0:
+            col6 = float(col9) / float(col4)
 
         # Agregar datos al Treeview en la ventana principal
         ventana_principal = self.master
-        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8))
+        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8,col9))
 
         self.destroy()  # Cerrar ventana
 
@@ -216,7 +209,7 @@ class VentanaOpcion2(tk.Toplevel):
         if not new_text:
             return True
         try:
-            if int(new_text) > 0 and int(new_text) <= 1000:
+            if int(new_text) > 0 and int(new_text) <= 10500:
                 return True
             else:
                 return False
@@ -230,10 +223,10 @@ class VentanaOpcion3(tk.Toplevel):
 
         # Crear campos de entrada
         vcmd = (self.register(self.validate_entry), '%P')
-        self.label_col2 = CTkLabel(self, text="Salidas:", text_color="Black")
-        self.label_col2.grid(row=0, column=0, padx=5, pady=5)
-        self.entry_col2 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col2.grid(row=0, column=1, padx=5, pady=5)
+        self.label_col3 = CTkLabel(self, text="Salidas:", text_color="Black")
+        self.label_col3.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_col3 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col3.grid(row=0, column=1, padx=5, pady=5)
 
         # Crear campo de fecha
         self.label_fecha = CTkLabel(self, text="Fecha:", text_color="Black")
@@ -246,39 +239,39 @@ class VentanaOpcion3(tk.Toplevel):
         self.btn_guardar.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
     def guardar_datos(self):
-        if not all([self.entry_col2.get()]):
+        if not all([self.entry_col3.get()]):
             messagebox.showerror("Error", "Ningún campo debe estar vacío.", parent=self)
             return
         selected_item = self.master.treeview.focus()
         if not selected_item:
             messagebox.showerror("Error", "Debe seleccionar una fila.", parent=self)
             return
-
-        col2 = self.entry_col2.get()
-        col1 = 0
-        col3 = float(self.master.treeview.item(self.master.treeview.focus())["values"][3]) - float(col2)
-        col4 = self.master.treeview.item(self.master.treeview.focus())["values"][5]
-        col5 = 0
+        col1= "COSTO DE VENTAS"
+        col3 = self.entry_col3.get()
+        col2 = 0
+        col4 = float(self.master.treeview.item(self.master.treeview.focus())["values"][4]) - float(col3)
+        col5 = self.master.treeview.item(self.master.treeview.focus())["values"][6]
         col6 = 0
-        col7 = float(col2) * float(col4)
-        col8 = float(self.master.treeview.item(self.master.treeview.focus())["values"][8]) - float(col7)
+        col7 = 0
+        col8 = float(col3) * float(col5)
+        col9 = float(self.master.treeview.item(self.master.treeview.focus())["values"][9]) - float(col8)
         selected_date = self.date_entry.get_date()
 
         selected_date = self.date_entry.get_date()
         formatted_date = selected_date.strftime("%Y-%m-%d")
         # Lógica para obtener col5
-        if col3 != 0:
-            col5 = float(col8) / float(col3)
+        if col4 != 0:
+            col6 = float(col9) / float(col4)
 
         # Agregar datos al Treeview en la ventana principal
         ventana_principal = self.master
-        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8))
+        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8,col9))
         self.destroy()
     def validate_entry(self, new_text):
         if not new_text:
             return True
         try:
-            if int(new_text) > 0 and int(new_text) <= 1000:
+            if int(new_text) > 0 and int(new_text) <= 10500:
                 return True
             else:
                 return False
@@ -314,31 +307,32 @@ class VentanaOpcion4(tk.Toplevel):
         if not selected_item:
             messagebox.showerror("Error", "Debe seleccionar una fila.", parent=self)
             return
-        col2 = int(self.entry_col3.get())
-        col1 = 0
-        col3 = float(self.master.treeview.item(self.master.treeview.focus())["values"][3]) - float(col2)
-        col4 = self.master.treeview.item(self.master.treeview.focus())["values"][5]
-        col5 = 0
+        col1= "DEVOLUCION S/C"
+        col3 = int(self.entry_col3.get())
+        col2 = 0
+        col4 = float(self.master.treeview.item(self.master.treeview.focus())["values"][4]) - float(col3)
+        col5 = self.master.treeview.item(self.master.treeview.focus())["values"][5]
         col6 = 0
-        col7 = float(col2) * float(col4)
-        col8 = float(self.master.treeview.item(self.master.treeview.focus())["values"][8]) - float(col7)
-        # Lógica para obtener col5
+        col7 = 0
+        col8 = float(col3) * float(col5)
+        col9 = float(self.master.treeview.item(self.master.treeview.focus())["values"][9]) - float(col8)
+
         selected_date = self.date_entry.get_date()
 
         formatted_date = selected_date.strftime("%Y-%m-%d")
-        if col3 != 0:
-            col5 = float(col8) / float(col3)
+        if col4 != 0:
+            col6 = float(col9) / float(col4)
 
         # Agregar datos al Treeview en la ventana principal
         ventana_principal = self.master
-        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8))
+        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8,col9))
 
         self.destroy()
     def validate_entry(self, new_text):
         if not new_text:
             return True
         try:
-            if int(new_text) > 0 and int(new_text) <= 1000:
+            if int(new_text) > 0 and int(new_text) <= 10500:
                 return True
             else:
                 return False
@@ -352,10 +346,10 @@ class VentanaOpcion5(tk.Toplevel):
 
         # Crear campos de entrada
         vcmd = (self.register(self.validate_entry), '%P')
-        self.label_col3 = CTkLabel(self, text="Entradas:", text_color="Black")
-        self.label_col3.grid(row=0, column=0, padx=5, pady=5)
-        self.entry_col3 = CTkEntry(self, validate='key', validatecommand=vcmd)
-        self.entry_col3.grid(row=0, column=1, padx=5, pady=5)
+        self.label_col2 = CTkLabel(self, text="Entradas:", text_color="Black")
+        self.label_col2.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_col2 = CTkEntry(self, validate='key', validatecommand=vcmd)
+        self.entry_col2.grid(row=0, column=1, padx=5, pady=5)
 
         self.label_fecha = CTkLabel(self, text="Fecha:", text_color="Black")
         self.label_fecha.grid(row=2, column=0, padx=5, pady=5)
@@ -367,37 +361,38 @@ class VentanaOpcion5(tk.Toplevel):
         self.btn_guardar.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
     def guardar_datos(self):
-        if not all([self.entry_col3.get()]):
+        if not all([self.entry_col2.get()]):
             messagebox.showerror("Error", "Ningún campo debe estar vacío.", parent=self)
             return
         selected_item = self.master.treeview.focus()
         if not selected_item:
             messagebox.showerror("Error", "Debe seleccionar una fila.", parent=self)
             return
-        col1 = int(self.entry_col3.get())
-        col2 = 0
-        col3 = float(self.master.treeview.item(self.master.treeview.focus())["values"][3]) + float(col1)
-        col4 = self.master.treeview.item(self.master.treeview.focus())["values"][5]
-        col5 = 0
-        col7 = 0
-        col6 = float(col1) * float(col4)
-        col8 = float(self.master.treeview.item(self.master.treeview.focus())["values"][8]) + float(col6)
+        col1="DEVOLUCION S/V"
+        col2 = int(self.entry_col2.get())
+        col3 = 0
+        col4 = float(self.master.treeview.item(self.master.treeview.focus())["values"][4]) + float(col2)
+        col5 = self.master.treeview.item(self.master.treeview.focus())["values"][6]
+        col6 = 0
+        col8 = 0
+        col7 = float(col2) * float(col5)
+        col9 = float(self.master.treeview.item(self.master.treeview.focus())["values"][9]) + float(col7)
         selected_date = self.date_entry.get_date()
 
         formatted_date = selected_date.strftime("%Y-%m-%d")
         # Lógica para obtener col5
-        if col3 != 0:
-            col5 = float(col8) / float(col3)
+        if col4 != 0:
+            col6 = float(col9) / float(col4)
 
         # Agregar datos al Treeview en la ventana principal
         ventana_principal = self.master
-        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8))
+        ventana_principal.treeview.insert("", "end",values=(formatted_date, col1, col2, col3, col4, col5, col6, col7, col8,col9))
         self.destroy()
     def validate_entry(self, new_text):
         if not new_text:
             return True
         try:
-            if int(new_text) > 0 and int(new_text) <= 1000:
+            if int(new_text) > 0 and int(new_text) <= 10500:
                 return True
             else:
                 return False
